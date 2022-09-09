028a7192981dd0e7c18096cfc9152bdabb4a992f
thunder6230 <62564512+thunder6230@users.noreply.github.com>
2022-09-09 08:29:22 +0200

    output file rename

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    rollupOptions:{
+      output:{
+        assetFileNames: "[name].[ext]",
+        chunkFileNames: "[name].js",
+        entryFileNames:  "[name].js"
+      }
+    }
+  },
   server: {
     port: 3000
   }
